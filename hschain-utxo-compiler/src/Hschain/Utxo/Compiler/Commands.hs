9cdc1ce4d6f26c663fd490213c5cdf7c4352e6cc
anton-k <anton.kholomiov@gmail.com>
2020-01-27 16:32:35 +0300

    Implements command to sign sigma-expressions with compiler

--- hschain-utxo-compiler/src/Hschain/Utxo/Compiler/Commands.hs
+++ hschain-utxo-compiler/src/Hschain/Utxo/Compiler/Commands.hs
@@ -6,6 +6,7 @@ module Hschain.Utxo.Compiler.Commands(
 ) where
 
 import Data.Aeson
+import Data.Maybe
 import Data.String
 
 import Data.ByteString.Lazy (ByteString)
@@ -14,6 +15,7 @@ import Hschain.Utxo.Lang.Parser.Hask
 import Type.Type
 import Type.Pretty
 import Hschain.Utxo.Lang.Expr
+import Hschain.Utxo.Lang.Exec
 import Hschain.Utxo.Lang.Types
 import Hschain.Utxo.Lang.Pretty
 import Hschain.Utxo.Lang.Desugar
@@ -72,11 +74,11 @@ genPrivateKey output = do
 getPublicKey :: FilePath -> Maybe FilePath -> IO ()
 getPublicKey input output = do
   eSecret <- fmap S.deserialiseOrFail $ LB.readFile input
-  either (const failToRead) onSecret eSecret
+  either (const failToReadInput) onSecret eSecret
   where
     onSecret secret = saveKey output $ Sigma.publicKeyToText $ Sigma.getPublicKey secret
 
-    failToRead = error $ mconcat ["Failed to read secret from file: ", input]
+    failToReadInput = failToRead "secret" input
 
     saveKey = maybe T.putStrLn T.writeFile
 
@@ -84,7 +86,34 @@ getPublicKey input output = do
 -- sign sigma expression
 
 signSigma :: FilePath -> FilePath -> Maybe FilePath -> IO ()
-signSigma secretFile input output = undefined
+signSigma secretFile input output = do
+  secret <- readSecret
+  expr   <- readInput
+  case expr of
+    ConstBool _     -> errorExpressionIsConst
+    SigmaBool sigma -> do
+      let env = Sigma.proofEnvFromKeys [Sigma.getKeyPair secret]
+      signedSigma <- Sigma.newProof env sigma
+      saveSigma signedSigma
+  where
+    readSecret :: IO Sigma.Secret
+    readSecret = do
+      file <- LB.readFile secretFile
+      return $ either (const failToReadSecret) id $ S.deserialiseOrFail file
+
+    readInput :: IO BoolExprResult
+    readInput = do
+      file <- LB.readFile input
+      return $ fromMaybe failToReadExpression $ decode' file
+
+    saveSigma = maybe C.putStrLn LB.writeFile output . encode
+
+    failToReadSecret     = failToRead "secret"     secretFile
+    failToReadExpression = failToRead "expression" input
+
+    errorExpressionIsConst = error "Expression os consant boolean, not a sigma expression. Nothing to sign."
 
 
+failToRead :: String -> FilePath -> a
+failToRead item file = error $ mconcat ["Failed to read ", item, " from file: ", file]
 
