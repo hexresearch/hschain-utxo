bc92f141b949dd67f04cb617a1bbd5995581adff
Serguey Zefirov <sergueyz@gmail.com>
2020-06-15 18:54:40 +0300

    Refactor web node - started; fix cabal complaint

---
+++ hschain-utxo-web-service/src/Hschain/Utxo/Back/Env.hs
@@ -0,0 +1,66 @@
+-- | Defines state of the node service
+module Hschain.Utxo.Back.Env where
+
+import Control.Concurrent.STM
+import Control.Monad
+import Control.Monad.Cont
+import Control.Monad.Catch
+
+import Data.Text (Text)
+import Data.Maybe                (fromMaybe)
+import Data.Time.Clock           (getCurrentTime)
+import Data.String
+
+import System.FilePath
+import System.Directory
+
+import HSChain.Store
+
+import Hschain.Utxo.Lang
+import Hschain.Utxo.Back.Config
+import Hschain.Utxo.State.React
+import Hschain.Utxo.State.Types
+import Hschain.Utxo.Blockchain
+
+import GHC.Generics (Generic)
+import Katip
+import qualified Data.Text as T
+
+import HSChain.Logger
+
+import Hschain.Utxo.Back.Config (LogSpec(..))
+
+-- | Application environment
+data AppEnv = AppEnv
+  { appEnv'bchain :: Bchain IO  -- ^ bridge to hschain internals
+  }
+
+-- | Setup service environment.
+--
+-- It takes node-specification and genesis.
+initEnv :: (MonadIO m, MonadMask m) => NodeSpec -> Genesis -> ContT r m (AppEnv, [IO ()])
+initEnv nspec genesis = do
+  (conn, logEnv) <- allocNode nspec
+  liftIO $ initEnvBy conn logEnv nspec genesis
+
+initEnvBy :: Connection 'RW BData -> LogEnv -> NodeSpec -> [Tx] -> IO (AppEnv, [IO ()])
+initEnvBy conn logenv nspec genesis = do
+  (bchain, acts) <- run $ interpretSpec (const $ pure ()) nspec genesis
+  return $ (AppEnv $ hoistBchain run bchain, fmap run acts)
+  where
+    run :: DBT 'RW BData (LoggerT m) x -> m x
+    run = runLoggerT logenv . runDBT conn
+
+-- | Allocate resources for node
+allocNode
+  :: ( MonadIO m, MonadMask m)
+  => NodeSpec
+  -> ContT r m (Connection 'RW a, LogEnv)
+allocNode NodeSpec{..} = do
+  mapM_ (\db -> liftIO $ createDirectoryIfMissing True $ takeDirectory db) nspec'dbName
+  conn   <- ContT $ withDatabase $ fromMaybe ":memory:" nspec'dbName
+  logenv <- ContT $ withLogEnv namespace "DEV" [ makeScribe s | s <- logSpec'files nspec'logs ]
+  return (conn,logenv)
+  where
+    namespace = fromString $ T.unpack $ logSpec'namespace nspec'logs
+
