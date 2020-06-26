0dbcf8e9aab5d7b1af20dcc5b283142de5338beb
anton-k <anton.kholomiov@gmail.com>
2020-06-26 18:40:53 +0300

    Completes missing conversion for core-lang inference

--- hindley-milner-tags/src/Language/HM.hs
+++ hindley-milner-tags/src/Language/HM.hs
@@ -8,3 +8,4 @@ import Language.HM.Pretty      as X()
 import Language.HM.Term        as X
 import Language.HM.Type        as X
 import Language.HM.TypeError   as X
+import Language.HM.TyTerm      as X
--- hschain-utxo-lang/src/Hschain/Utxo/Lang/Compile/Infer.hs
+++ hschain-utxo-lang/src/Hschain/Utxo/Lang/Compile/Infer.hs
@@ -51,6 +51,9 @@ instance H.IsPrim Prim where
 eraseLoc :: Type -> H.Type Loc Tag
 eraseLoc = H.mapLoc (const noLoc) . fmap VarTag
 
+toType :: H.Type Loc Tag -> Type
+toType = H.mapLoc (const ()) . fmap fromTag
+
 -- | Infers types for all subexpressions
 annotateTypes :: forall m . MonadLang m => CoreProg -> m TypedProg
 annotateTypes = fmap (reverse . snd) . foldM go (mempty, []) . orderDependencies
@@ -62,9 +65,10 @@ annotateTypes = fmap (reverse . snd) . foldM go (mempty, []) . orderDependencies
     typeDef :: Context -> Comb Name -> m (H.Type Loc Tag, AnnComb Type (Typed Name))
     typeDef ctx comb = do
       (combT, term) <- liftEither $ either fromErr Right $ H.inferTerm ctx (toInferExpr $ getCombExpr comb)
+      body <- fromInferExpr term
       return $ (combT, comb
         { def'args = []
-        , def'body = fromInferExpr term
+        , def'body = body
         })
 
     fromErr = Left . TypeError . fmap fromTag
@@ -105,5 +109,45 @@ annotateTypes = fmap (reverse . snd) . foldM go (mempty, []) . orderDependencies
         -- we need to know the types of the constructors on this stage:
         toArg (Typed val ty) = H.Typed (eraseLoc ty) (VarTag val)
 
-    fromInferExpr = undefined
+    fromInferExpr :: H.TyTerm Prim Loc Tag -> m (AnnExpr Type (Typed Name))
+    fromInferExpr (H.TyTerm x) = flip cataM x $ \case
+      H.Ann ty expr -> fmap (Fix . Ann (toType ty)) $ case expr of
+        H.Var loc name -> pure $ EVar loc (fromTag name)
+        H.Prim loc p   -> pure $ EPrim loc p
+        H.App _ (Fix (Ann _ (EAp _ (Fix (Ann _ (EAp _ (Fix (Ann _ (EVar vloc "if"))) c))) t))) e -> pure $ EIf vloc c t e
+        H.App loc a b -> pure $ EAp loc a b
+        H.Lam loc arg e -> fmap (\t -> ELam loc [Typed (fromTag arg) (toType t)] e) $ getLamArgType ty
+        H.Bottom loc -> pure $ EBottom loc
+        H.AssertType _ (Fix (Ann _ a)) _ -> pure $ a
+        H.Let loc bs e -> toLet loc bs e
+        H.LetRec loc bs e -> toLet loc bs e
+        H.Case loc e alts -> fmap (ECase loc e) (mapM toAlt alts)
+        H.Constr loc conTy tag n ->
+          case tag of
+            ConstrTag m -> pure $ EConstr loc (toType conTy) m n
+            _           -> throwError $ InternalError $ NonIntegerConstrTag (fromTag tag)
+
+    toAlt alt =
+      case H.caseAlt'tag alt of
+        ConstrTag tagId -> pure $
+          CaseAlt
+            { caseAlt'loc        = H.caseAlt'loc alt
+            , caseAlt'tag        = tagId
+            , caseAlt'args       = fmap (\t -> Typed (fromTag $ H.typed'value t) (toType $ H.typed'type t)) $ H.caseAlt'args alt
+            , caseAlt'constrType = toType $ H.caseAlt'constrType alt
+            , caseAlt'rhs        = H.caseAlt'rhs alt
+            }
+        other -> throwError $ InternalError $ NonIntegerConstrTag (fromTag other)
+
+
+    toLet loc binds body = pure $ ELet loc (fmap toBind binds) body
+
+    toBind b = (Typed (fromTag $ H.bind'lhs b) (getType rhs), rhs)
+      where
+        rhs = H.bind'rhs b
+        getType (Fix (Ann t _)) = t
+
+    getLamArgType (H.Type (Fix x)) = case x of
+      H.ArrowT _ a _ -> pure $ H.Type a
+      _              -> throwError $ InternalError $ NonLamType
 
