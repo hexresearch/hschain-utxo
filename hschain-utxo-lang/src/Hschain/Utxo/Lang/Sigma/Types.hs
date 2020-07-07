59ff663378d2e7602694948f4aa2f5e13e652b55
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2020-07-07 16:34:54 +0300

    Add newtype CryptoHashable instances for Sectet/Public keys

--- hschain-utxo-lang/src/Hschain/Utxo/Lang/Sigma/Types.hs
+++ hschain-utxo-lang/src/Hschain/Utxo/Lang/Sigma/Types.hs
@@ -1,24 +1,23 @@
 -- | Module defines main types for sigma-expressions.
-module Hschain.Utxo.Lang.Sigma.Types
-where
-
-import Control.DeepSeq (NFData)
-
-import Data.Coerce
-
-import GHC.Generics (Generic)
-
-import Hschain.Utxo.Lang.Sigma.EllipticCurve
+module Hschain.Utxo.Lang.Sigma.Types where
 
 import qualified Codec.Serialise as CBOR
+import Control.DeepSeq (NFData)
+import Data.Coerce
+import GHC.Generics (Generic)
+
+import HSChain.Crypto.Classes.Hash
+import Hschain.Utxo.Lang.Sigma.EllipticCurve
+
+
 
 -- | Private key.
 newtype Secret    a = Secret    { unSecret    :: ECScalar a
   } deriving (Generic)
 
 -- | Public key.
-newtype PublicKey a = PublicKey { unPublicKey :: ECPoint  a
-  } deriving (Generic)
+newtype PublicKey a = PublicKey { unPublicKey :: ECPoint a }
+  deriving (Generic)
 
 -- | Pair of keys.
 data KeyPair a = KeyPair
@@ -35,6 +34,7 @@ deriving stock   instance Ord  (ECPoint a) => Ord  (PublicKey a)
 deriving newtype instance (CBOR.Serialise (ECScalar a)) => CBOR.Serialise (Secret a)
 deriving newtype instance (CBOR.Serialise (ECPoint a)) => CBOR.Serialise (PublicKey a)
 deriving newtype instance NFData (ECPoint a) => NFData (PublicKey a)
+deriving newtype instance (CryptoHashable (ECPoint a)) => CryptoHashable (PublicKey a)
 
 -- | Generate new private key.
 generateSecretKey :: EC a => IO (Secret a)
@@ -53,3 +53,4 @@ generateKeyPair = do
 deriving instance Show (ECScalar a) => Show (Secret a)
 deriving instance Eq   (ECScalar a) => Eq   (Secret a)
 deriving instance Ord  (ECScalar a) => Ord  (Secret a)
+deriving newtype instance (CryptoHashable (ECScalar a)) => CryptoHashable (Secret a)
