e48e948fe0f9628be2d86b7f53b08fb11ffc51f3
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2020-07-09 15:47:42 +0300

    Move SKI into separate module
    To be reused by benchmarks

---
+++ hschain-utxo-lang/examples/Examples/SKI.hs
@@ -0,0 +1,83 @@
+{-# LANGUAGE OverloadedLists   #-}
+{-# LANGUAGE OverloadedStrings #-}
+-- |
+-- SKI calculus examples
+module Examples.SKI where
+
+import Data.Text (Text)
+
+import Hschain.Utxo.Lang.Core.Compile
+import Hschain.Utxo.Lang.Core.Data.Prim
+
+
+----------------------------------------------------------------
+-- Combinators
+----------------------------------------------------------------
+
+-- | I Combinator
+--
+-- > I :: a -> a
+-- > I x = x
+skiI :: Name -> Type -> Scomb
+skiI name ty = Scomb
+  { scomb'name = "skiI." <> name
+  , scomb'args = [Typed "x" ty]
+  , scomb'body = Typed (EVar "x") ty
+  }
+
+-- | K combinator
+--
+-- > K :: a -> b -> a
+-- > K x y = x
+skiK :: Text -> Type -> Type -> Scomb
+skiK name tyX tyY = Scomb
+  { scomb'name = "skiK." <> name
+  , scomb'args = [Typed "x" tyX, Typed "y" tyY]
+  , scomb'body = Typed (EVar "x") tyX
+  }
+
+-- | S combinator
+--
+-- > S :: (a -> b -> c) -> (a -> b) -> a -> c
+-- > S x y z = x z (y z)
+skiS :: Text -> Type -> Type -> Type -> Scomb
+skiS name tyA tyB tyC = Scomb
+  { scomb'name = "skiS." <> name
+  , scomb'args = [ Typed "x" (tyA `arrowT` (tyB `arrowT` tyC))
+                 , Typed "y" (tyA `arrowT` tyB)
+                 , Typed "z" tyA
+                 ]
+  , scomb'body = Typed
+                 ((EAp (EVar "x") (EVar "z")) `EAp` (EAp (EVar "y") (EVar "z")))
+                 tyC
+  }
+
+
+----------------------------------------------------------------
+-- Example programs
+----------------------------------------------------------------
+
+-- | Example of program
+--
+-- > S K K 3
+exampleSKK3 :: [Scomb]
+exampleSKK3 =
+  [ skiK "intT" intT intT
+  , skiK "funT" intT (intT `arrowT` intT)
+  , skiS ""     intT (intT `arrowT` intT) intT
+  , mkMain $ Typed
+    (((EVar "skiS." `EAp` EVar "skiK.funT") `EAp` EVar "skiK.intT") `EAp` EPrim (PrimInt 3))
+    intT
+  ]
+
+
+----------------------------------------------------------------
+-- Helpers
+----------------------------------------------------------------
+
+mkMain :: Typed Expr -> Scomb
+mkMain s = Scomb
+  { scomb'name = "main"
+  , scomb'args = mempty
+  , scomb'body = s
+  }
--- hschain-utxo-lang/test/TM/Core.hs
+++ hschain-utxo-lang/test/TM/Core.hs
@@ -4,7 +4,6 @@
 -- |
 module TM.Core ( tests )where
 
-import Data.Text (Text)
 import Test.Tasty
 import Test.Tasty.HUnit
 
@@ -13,6 +12,8 @@ import Hschain.Utxo.Lang.Core.Compile.Primitives
 import Hschain.Utxo.Lang.Core.Data.Prim
 import Hschain.Utxo.Lang.Core.Gmachine
 import qualified Hschain.Utxo.Lang.Core.Data.Output as O
+import Examples.SKI
+
 
 tests :: TestTree
 tests = testGroup "core"
@@ -50,65 +51,6 @@ progAddition =
     , typed'type  = intT
     }
   ]
-
-mkMain :: Typed Expr -> Scomb
-mkMain s = Scomb
-  { scomb'name = "main"
-  , scomb'args = mempty
-  , scomb'body = s
-  }
-
-
-----------------------------------------------------------------
--- SKI-calculus
-
--- I :: a -> a
--- I x = x
-skiI :: Type -> Scomb
-skiI ty = Scomb
-  { scomb'name = "skiI" -- ++ show ty
-  , scomb'args = [Typed "x" ty]
-  , scomb'body = Typed (EVar "x") ty
-  }
-
--- K :: a -> b -> a
--- K x y = x
-skiK :: Text -> Type -> Type -> Scomb
-skiK name tyX tyY = Scomb
-  { scomb'name = "skiK." <> name
-  , scomb'args = [Typed "x" tyX, Typed "y" tyY]
-  , scomb'body = Typed (EVar "x") tyX
-  }
-
--- S :: (a -> b -> c) -> (a -> b) -> a -> c
--- S x y z = x z (y z)
-skiS :: Type -> Type -> Type -> Scomb
-skiS tyA tyB tyC = Scomb
-  { scomb'name = "skiS"
-  , scomb'args = [ Typed "x" (tyA `arrowT` (tyB `arrowT` tyC))
-                 , Typed "y" (tyA `arrowT` tyB)
-                 , Typed "z" tyA
-                 ]
-  , scomb'body = Typed
-                 ((EAp (EVar "x") (EVar "z")) `EAp` (EAp (EVar "y") (EVar "z")))
-                 tyC
-  }
-
--- Example of program
---
--- > S K K 3
-exampleSKK3 :: [Scomb]
-exampleSKK3 =
-  [ skiK "intT" intT intT
-  , skiK "funT" intT (intT `arrowT` intT)
-  , skiS        intT (intT `arrowT` intT) intT
-  , mkMain $ Typed
-    (((EVar "skiS" `EAp` EVar "skiK.funT") `EAp` EVar "skiK.intT") `EAp` EPrim (PrimInt 3))
-    intT
-  ]
-
-
-
 
 run :: CoreProg -> Either Error [Prim]
 run
