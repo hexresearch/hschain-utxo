fb6a83ce9a3de07fdc007104b858faf90641602b
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2020-07-16 17:07:08 +0300

    Add simple tests for checking equality
    Also add first version of test that does something with TX environment

--- hschain-utxo-lang/test/TM/Core.hs
+++ hschain-utxo-lang/test/TM/Core.hs
@@ -5,11 +5,12 @@
 module TM.Core ( tests )where
 
 import Data.Fix
-
 import Test.Tasty
 import Test.Tasty.HUnit
 
 import Hschain.Utxo.Lang.Sigma
+import Hschain.Utxo.Lang.Expr  (Box(..),BoxId(..),Script(..),Args(..))
+import Hschain.Utxo.Lang.Types (TxEnv(..))
 import Hschain.Utxo.Lang.Core.Compile
 import Hschain.Utxo.Lang.Core.Compile.Primitives
 import Hschain.Utxo.Lang.Core.Data.Prim
@@ -25,12 +26,20 @@ tests = testGroup "core"
     , testProgram "Addition"     progAddition   (PrimInt 101)
     , testProgram "SKK3"         exampleSKK3    (PrimInt 3)
     ]
+  , testGroup "primitives"
+    [ testProgram "eq.Int"  (progEquality (PrimInt  12))    (PrimBool True)
+    , testProgram "eq.Bool" (progEquality (PrimBool False)) (PrimBool True)
+    , testProgram "eq.Int"  (progEquality (PrimText "12"))  (PrimBool True)
+    ]
+  , testGroup "env"
+    [ testProgram "getHeight" progHeight (PrimInt 123)
+    ]
   ]
 
 testProgram :: String -> CoreProg -> Prim -> TestTree
 testProgram nm prog res = testGroup nm
   [ testCase "typecheck" $ Nothing     @=? typeCheck preludeTypeContext prog
-  , testCase "eval"      $ Right [res] @=? run prog
+  , testCase "eval"      $ Right [res] @=? run (prog <> CoreProg (environmentFunctions env))
   ]
 
 
@@ -42,6 +51,27 @@ progSpendToKey = CoreProg
     , typed'type  = sigmaT
     }
   ]
+
+progHeight :: CoreProg
+progHeight = CoreProg
+  [ mkMain $ Typed
+    { typed'value = EVar (Typed "getHeight" intT)
+    , typed'type  = intT
+    }
+  ]
+
+progEquality :: Prim -> CoreProg
+progEquality p = CoreProg
+  [ mkMain $ Typed
+    { typed'value =
+        (EVar (Typed eq (funT [ty,ty] boolT)) `EAp` EPrim p) `EAp` EPrim p
+    , typed'type  = boolT
+    }
+  ]
+  where
+    ty = primToType p
+    eq = toCompareName ty "equals"
+
 
 -- Addition of two integers
 progAddition :: CoreProg
@@ -61,3 +91,24 @@ run
   = fmap (O.toList . gmachine'output)
   . eval
   . compile
+
+
+----------------------------------------------------------------
+
+env :: TxEnv
+env = TxEnv
+  { txEnv'height   = 123
+  , txEnv'self     = Box
+    { box'id     = BoxId ""
+    , box'value  = 100
+    , box'script = Script ""
+    , box'args   = Args
+      { args'ints  = mempty
+      , args'bools = mempty
+      , args'texts = mempty
+      }
+    }
+  , txEnv'inputs   = mempty
+  , txEnv'outputs  = mempty
+  , txEnv'args     = mempty
+  }
