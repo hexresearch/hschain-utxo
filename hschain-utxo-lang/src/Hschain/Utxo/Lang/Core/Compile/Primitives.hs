9e2d201d734f1d4da15a9bc64100c392c1d6fa06
anton-k <anton.kholomiov@gmail.com>
2020-07-20 14:47:50 +0300

    Simple test for list-functions

--- hschain-utxo-lang/src/Hschain/Utxo/Lang/Core/Compile/Primitives.hs
+++ hschain-utxo-lang/src/Hschain/Utxo/Lang/Core/Compile/Primitives.hs
@@ -16,13 +16,13 @@ import Data.Text (Text)
 import Data.Vector (Vector)
 
 import Hschain.Utxo.Lang.Expr (Box(..), BoxId(..), Script(..), Args(..), ArgType(..), argTypeName)
+import Hschain.Utxo.Lang.Core.Compile.Build
 import Hschain.Utxo.Lang.Core.Compile.Expr
 import Hschain.Utxo.Lang.Core.Compile.TypeCheck
 import Hschain.Utxo.Lang.Core.Data.Code (Instr(..))
 import Hschain.Utxo.Lang.Core.Data.Prim
 import Hschain.Utxo.Lang.Types (TxEnv(..))
 
-import qualified Data.List as L
 import qualified Data.Map.Strict as M
 import qualified Data.Vector as V
 
@@ -88,6 +88,8 @@ primitives =
   , op1 "hashSha" textT textT
 
   -- lists
+  , nilComb
+  , consComb
   , foldrComb
   , mapComb
   , listAppendComb
@@ -124,47 +126,6 @@ comparePack ty =
   , compareOp ty (toCompareName ty "lessThan")
   , compareOp ty (toCompareName ty "lessThanEquals")
   ]
-
-ap :: ExprCore -> [ExprCore] -> ExprCore
-ap f args = L.foldl' (\op a -> EAp op a) f args
-
--- | Application of function to two arguments
-ap2 :: ExprCore -> ExprCore -> ExprCore -> ExprCore
-ap2 f a b = EAp (EAp f a) b
-
-constant :: Name -> Prim -> Scomb
-constant name val = Scomb
-  { scomb'name = name
-  , scomb'args = V.empty
-  , scomb'body = Typed (EPrim val) (primToType val)
-  }
-
-op1 :: Name -> TypeCore -> TypeCore -> Scomb
-op1 name argT resT = Scomb
-  { scomb'name = name
-  , scomb'args = V.fromList $ [Typed "x" argT]
-  , scomb'body = Typed (EAp (EVar $ Typed name (arrowT argT resT)) (EVar $ Typed "x" argT)) resT
-  }
-
-intOp2 :: Name -> Scomb
-intOp2 name = op2 name (intT, intT) intT
-
-boolOp2 :: Name -> Scomb
-boolOp2 name = op2 name (boolT, boolT) boolT
-
-sigmaOp2 :: Name -> Scomb
-sigmaOp2 name = op2 name (sigmaT, sigmaT) sigmaT
-
--- | TODO: do we need polymorphic comparison?
-compareOp :: TypeCore -> Name -> Scomb
-compareOp ty name = op2 name (ty, ty) boolT
-
-op2 :: Name -> (TypeCore, TypeCore) -> TypeCore -> Scomb
-op2 name (xT, yT) resT = Scomb
-  { scomb'name = name
-  , scomb'args = V.fromList [Typed "x" xT, Typed "y" yT]
-  , scomb'body = Typed (ap2 (EVar $ Typed name (funT [xT, yT] resT)) (EVar $ Typed "x" xT) (EVar $ Typed "y" yT)) resT
-  }
 
 ------------------------------------------------------------
 -- boxes
@@ -304,6 +265,36 @@ getArgs Args{..} =
       }
 ------------------------------------------------------------
 -- lists
+
+nilComb :: Scomb
+nilComb = Scomb
+  { scomb'name = "nil"
+  , scomb'args = V.empty
+  , scomb'body = Typed
+      (EConstr nilTy 0 0)
+      nilTy
+  }
+  where
+    nilTy = listT (varT "a")
+
+consComb :: Scomb
+consComb = Scomb
+  { scomb'name = "cons"
+  , scomb'args = V.fromList [x, xs]
+  , scomb'body = Typed
+      (ap (EConstr consT 1 2) [xv, xsv])
+      asT
+  }
+  where
+    x  = Typed "x" aT
+    xs = Typed "xs" asT
+    consT = funT [aT, asT] asT
+
+    xv = EVar x
+    xsv = EVar xs
+
+    aT  = varT "a"
+    asT = listT aT
 
 foldrComb :: Scomb
 foldrComb = Scomb
--- hschain-utxo-lang/test/Main.hs
+++ hschain-utxo-lang/test/Main.hs
@@ -1,7 +1,9 @@
 import Test.Tasty
 import qualified TM.Core
+import qualified TM.Core.List
 
 main :: IO ()
 main = defaultMain $ testGroup "lang"
   [ TM.Core.tests
+  , TM.Core.List.tests
   ]
