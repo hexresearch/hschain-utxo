dbc60b953dd498ef4ed610daebfd3b1020b3db59
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2020-07-28 13:51:01 +0300

    Implement most of primitives

--- hschain-utxo-lang/src/Hschain/Utxo/Lang/Core/RefEval.hs
+++ hschain-utxo-lang/src/Hschain/Utxo/Lang/Core/RefEval.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE MonadComprehensions #-}
+{-# LANGUAGE RankNTypes          #-}
 -- |
 module Hschain.Utxo.Lang.Core.RefEval
   ( Val(..)
@@ -5,26 +7,41 @@ module Hschain.Utxo.Lang.Core.RefEval
   ) where
 
 import Data.Int
+import Data.ByteString (ByteString)
+import Data.Text       (Text)
+import Data.Fix
 import qualified Data.Vector     as V
+import qualified Data.Text       as T
 import qualified Data.Map.Strict as Map
 import qualified Data.Map.Lazy   as MapL
 
+import HSChain.Crypto     (Hash(..),hashBlob)
+import HSChain.Crypto.SHA (SHA256)
 import Hschain.Utxo.Lang.Core.Data.Prim
 import Hschain.Utxo.Lang.Core.Data.Code
 import Hschain.Utxo.Lang.Core.Compile.Expr
 import Hschain.Utxo.Lang.Core.Compile.Primitives
+import Hschain.Utxo.Lang.Sigma
 
 
-data Val = ValP !Prim
-         | ValBottom
-         | ValF (Val -> Val)
-         | ValCon Int [Val]
+-- | Value hanled by evaluator
+data Val
+  = ValP !Prim                  -- ^ Primitive value
+  | ValBottom                   -- ^ Bottom. Always terminate evaluation
+  | ValF  (Val -> Val)          -- ^ Unary function
+  | Val2F (Val -> Val -> Val)   -- ^ Binary function. Added in order to make defining primops easier
+  | ValCon Int [Val]            -- ^ Constructor cell
 
 
+-- | Global environment. Values for globally defined function.
 type GEnv = Map.Map Name Val
+
+-- | Loval evaluation environment. Map from supercombinator names to
+--   their evaluation.
 type LEnv = Map.Map Name Val
 
 
+-- | Evaluate program
 evalProg :: CoreProg -> Maybe Prim
 evalProg (CoreProg prog) = do
   ValP p <- "main" `Map.lookup` genv
@@ -32,19 +49,6 @@ evalProg (CoreProg prog) = do
   where
     genv = MapL.fromList [ (scomb'name s, evalScomb genv s) | s <- prog ]
 
-primVals :: Map.Map Name Val
-primVals = fmap evalD builtInDiadic
-  where
-    evalD = \case
-      Add -> arith2 (+)
-      _   -> error "Not implemented"
-
-arith2 :: (Int64 -> Int64 -> Int64) -> Val
-arith2 f = ValF $ \case
-  ValP (PrimInt a) -> ValF $ \case
-    ValP (PrimInt b) -> ValP (PrimInt (f a b))
-    _                -> ValBottom
-  _ -> ValBottom
 
 evalScomb :: GEnv -> Scomb -> Val
 evalScomb genv Scomb{..} = buildArg Map.empty (V.toList scomb'args)
@@ -65,8 +69,9 @@ evalExpr genv = recur
       EPolyVar x _ -> evalVar lenv x
       EPrim p -> ValP p
       EAp f x -> case recur lenv f of
-                   ValF valF -> valF $ recur lenv x
-                   _         -> ValBottom
+                   ValF  valF -> valF $ recur lenv x
+                   Val2F valF -> ValF $ valF $ recur lenv x
+                   _          -> ValBottom
       EIf e a b -> case recur lenv e of
         ValP (PrimBool f) -> recur lenv $ if f then a else b
         _                 -> ValBottom
@@ -92,8 +97,8 @@ evalExpr genv = recur
       EConstr _ tag arity    -> constr tag arity
       --
       EBottom{} -> ValBottom
-    --
 
+-- Generate constructor
 constr :: Int -> Int -> Val
 constr tag arity = build
   (\v xs -> xs . (v:))
@@ -106,3 +111,127 @@ build step fini = go
   where
     go a 0 = fini a
     go a i = ValF $ \v -> go (step v a) (i-1)
+
+
+----------------------------------------------------------------
+-- Primitives
+----------------------------------------------------------------
+
+primVals :: Map.Map Name Val
+primVals = fmap evalD builtInDiadic <> fmap evalD builtInUnary
+  where
+    evalD = \case
+      Add -> lift2 ((+) @Int64)
+      Mul -> lift2 ((*) @Int64)
+      Sub -> lift2 ((-) @Int64)
+      Div -> lift2 (div @Int64)
+      Neg -> lift1 (negate @Int64)
+      -- Polymorphic ops
+      Eq -> opComparison (==)
+      Ne -> opComparison (/=)
+      Lt -> opComparison (<)
+      Le -> opComparison (<=)
+      Gt -> opComparison (>)
+      Ge -> opComparison (>=)
+      --
+      And -> lift2 (&&)
+      Or  -> lift2 (||)
+      Xor -> lift2 $ \a b -> (a || b) && not (a && b)
+      Not -> lift1 not
+      --
+      SigAnd  -> lift2 $ \a b -> Fix $ SigmaAnd [a,b]
+      SigOr   -> lift2 $ \a b -> Fix $ SigmaOr  [a,b]
+      SigPk   -> lift1 $ \t   -> Fix . SigmaPk <$> publicKeyFromText t
+      SigBool -> lift1 $ Fix . SigmaBool
+      --
+      TextLength  -> lift1 (fromIntegral @_ @Int64 . T.length)
+      TextAppend  -> lift2 ((<>) @Text)
+      BytesAppend -> lift2 ((<>) @ByteString)
+      ToBytes{}   -> undefined
+      FromBytes{} -> undefined
+      HashBlake   -> error "Blake2b is not implemented yet"
+      HashSha     -> lift1 $ \bs -> let Hash h = hashBlob @SHA256 bs in h
+      Sha256      -> lift1 $ \bs -> let Hash h = hashBlob @SHA256 bs in h
+      ShowInt     -> lift1 (T.pack . show @Int64)
+      ShowBool    -> lift1 (T.pack . show @Bool)
+      Bottom      -> ValBottom
+      -- G-machine primitives      
+      Unwind       -> gerr
+      PushGlobal{} -> gerr
+      PushPrim{}   -> gerr
+      PushBasic{}  -> gerr
+      Push{}       -> gerr
+      Mkap         -> gerr
+      Update{}     -> gerr
+      Pop{}        -> gerr
+      Slide{}      -> gerr
+      Alloc{}      -> gerr
+      Eval         -> gerr
+      Cond{}       -> gerr
+      Pack{}       -> gerr
+      CaseJump{}   -> gerr
+      Split{}      -> gerr
+      Print        -> gerr
+      MkPrim       -> gerr
+      Get          -> gerr
+      UpdatePrim{} -> gerr
+    gerr = error "GMachine primitive"
+
+opComparison :: (forall a. Ord a => a -> a -> Bool) -> Val
+opComparison (#) = primFun2 go
+  where
+    go (PrimInt   a) (PrimInt   b) = ValP $ PrimBool $ a # b
+    go (PrimBool  a) (PrimBool  b) = ValP $ PrimBool $ a # b
+    go (PrimText  a) (PrimText  b) = ValP $ PrimBool $ a # b
+    go (PrimBytes a) (PrimBytes b) = ValP $ PrimBool $ a # b
+    -- FIXME: Comparison for sigma expressions?
+    go (PrimSigma _) (PrimSigma _) = ValBottom
+    go _ _ = ValBottom
+    
+primFun2 :: (Prim -> Prim -> Val) -> Val
+primFun2 f = Val2F go
+  where
+    go (ValP a) (ValP b) = f a b
+    go _        _        = ValBottom
+
+
+----------------------------------------------------------------
+-- Lifting of functions
+----------------------------------------------------------------
+
+class MatchPrim a where
+  matchP :: Prim -> Maybe a
+
+class InjPrim a where
+  inj :: a -> Val
+
+instance MatchPrim Int64      where  matchP p = [ a | PrimInt   a <- pure p ]
+instance MatchPrim Bool       where  matchP p = [ a | PrimBool  a <- pure p ]
+instance MatchPrim Text       where  matchP p = [ a | PrimText  a <- pure p ]
+instance MatchPrim ByteString where  matchP p = [ a | PrimBytes a <- pure p ]
+
+instance k ~ PublicKey => MatchPrim (Sigma k) where
+  matchP p = [ a | PrimSigma a <- pure p ]
+
+instance InjPrim Int64      where inj = ValP . PrimInt
+instance InjPrim Bool       where inj = ValP . PrimBool
+instance InjPrim Text       where inj = ValP . PrimText
+instance InjPrim ByteString where inj = ValP . PrimBytes
+
+instance k ~ PublicKey => InjPrim (Sigma k) where
+  inj = ValP . PrimSigma
+
+instance InjPrim a => InjPrim (Maybe a) where
+  inj = maybe ValBottom inj
+
+lift1 :: (MatchPrim a, InjPrim b) => (a -> b) -> Val
+lift1 f = ValF go
+  where
+    go (ValP a) = inj $ f <$> matchP a
+    go _        = ValBottom
+
+lift2 :: (MatchPrim a, MatchPrim b, InjPrim c) => (a -> b -> c) -> Val
+lift2 f = Val2F go
+  where
+    go (ValP a) (ValP b) = inj $ f <$> matchP a <*> matchP b
+    go _        _        = ValBottom
