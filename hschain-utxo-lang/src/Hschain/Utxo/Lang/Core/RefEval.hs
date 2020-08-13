0b3a445c2bbd28dfae700daef496b1ff103e64c7
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2020-08-13 18:10:00 +0300

    Add From/ToBytes

--- hschain-utxo-lang/src/Hschain/Utxo/Lang/Core/RefEval.hs
+++ hschain-utxo-lang/src/Hschain/Utxo/Lang/Core/RefEval.hs
@@ -8,6 +8,7 @@ module Hschain.Utxo.Lang.Core.RefEval
   , evalProg
   ) where
 
+import Codec.Serialise (Serialise,serialise,deserialiseOrFail)
 import Data.Int
 import Data.ByteString (ByteString)
 import Data.Text       (Text)
@@ -16,6 +17,7 @@ import qualified Data.Vector     as V
 import qualified Data.Text       as T
 import qualified Data.Map.Strict as Map
 import qualified Data.Map.Lazy   as MapL
+import qualified Data.ByteString.Lazy as LB
 
 import HSChain.Crypto     (Hash(..),hashBlob)
 import HSChain.Crypto.SHA (SHA256)
@@ -23,6 +25,7 @@ import Hschain.Utxo.Lang.Core.Data.Prim
 import Hschain.Utxo.Lang.Core.Data.Code
 import Hschain.Utxo.Lang.Core.Compile.Expr
 import Hschain.Utxo.Lang.Core.Compile.Primitives
+import Hschain.Utxo.Lang.Expr  (ArgType(..))
 import Hschain.Utxo.Lang.Sigma
 import Hschain.Utxo.Lang.Types (InputEnv)
 
@@ -182,8 +185,22 @@ primVals = fmap evalD builtInDiadic <> fmap evalD builtInUnary
       TextLength  -> lift1 (fromIntegral @_ @Int64 . T.length)
       TextAppend  -> lift2 ((<>) @Text)
       BytesAppend -> lift2 ((<>) @ByteString)
-      ToBytes{}   -> undefined
-      FromBytes{} -> undefined
+      ToBytes   tag -> case tag of
+        IntArg   -> lift1 $ serialise @Int64
+        TextArg  -> lift1 $ serialise @Text
+        BoolArg  -> lift1 $ serialise @Bool
+        BytesArg -> lift1 $ serialise @ByteString
+      FromBytes tag -> case tag of
+        IntArg   -> lift1 $ decode @Int64
+        TextArg  -> lift1 $ decode @Text
+        BoolArg  -> lift1 $ decode @Bool
+        BytesArg -> lift1 $ decode @ByteString
+        where
+          decode :: Serialise a => LB.ByteString -> Either EvalErr a
+          decode bs = case deserialiseOrFail bs of
+            Right a -> Right a
+            Left  _ -> Left $ EvalErr "Deserialize failed"
+      -- FromBytes tag -> deserialiseFromBytes tag
       HashBlake   -> error "Blake2b is not implemented yet"
       HashSha     -> lift1 $ \bs -> let Hash h = hashBlob @SHA256 bs in h
       Sha256      -> lift1 $ \bs -> let Hash h = hashBlob @SHA256 bs in h
@@ -256,15 +273,19 @@ instance MatchPrim Text where
 instance MatchPrim ByteString where
   matchP (PrimBytes a) = Right a
   matchP _             = Left $ EvalErr "Expecting Bytes"
+instance MatchPrim LB.ByteString where
+  matchP (PrimBytes a) = Right $ LB.fromStrict a
+  matchP _             = Left $ EvalErr "Expecting Bytes"
 
 instance k ~ PublicKey => MatchPrim (Sigma k) where
   matchP (PrimSigma a) = Right a
   matchP _             = Left $ EvalErr "Expecting Sigma"
 
-instance InjPrim Int64      where inj = ValP . PrimInt
-instance InjPrim Bool       where inj = ValP . PrimBool
-instance InjPrim Text       where inj = ValP . PrimText
-instance InjPrim ByteString where inj = ValP . PrimBytes
+instance InjPrim Int64         where inj = ValP . PrimInt
+instance InjPrim Bool          where inj = ValP . PrimBool
+instance InjPrim Text          where inj = ValP . PrimText
+instance InjPrim ByteString    where inj = ValP . PrimBytes
+instance InjPrim LB.ByteString where inj = inj . LB.toStrict
 
 instance k ~ PublicKey => InjPrim (Sigma k) where
   inj = ValP . PrimSigma
