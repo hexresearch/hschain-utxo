be08470ea2cd29563eb2d199ec8e34fbd05e124a
anton-k <anton.kholomiov@gmail.com>
2020-09-08 13:28:07 +0300

    Init atomic swap script

---
+++ hschain-utxo-test/src/Hschain/Utxo/Test/Client/Scripts/AtomicSwap.hs
@@ -0,0 +1,47 @@
+module Hschain.Utxo.Test.Client.Scripts.AtomicSwap where
+
+import Prelude hiding ((<*))
+
+import Data.ByteString (ByteString)
+
+import System.Random
+
+import Hschain.Utxo.Lang
+import Hschain.Utxo.Lang.Build
+import Hschain.Utxo.Lang.Utils.ByteString(getSha256)
+
+import Hschain.Utxo.Test.Client.Monad (App, testTitle)
+import Hschain.Utxo.Test.Client.Scripts.Utils
+
+import qualified Data.ByteString.Char8 as B
+
+atomicSwap :: App ()
+atomicSwap = do
+  testTitle "Atomic swap: Alice exhanges with Bob."
+  Scene{..} <- initUsers
+  return ()
+
+type SwapSecret = ByteString
+type SwapHash   = ByteString
+
+initSecret :: Int -> IO SwapSecret
+initSecret size = fmap B.pack $ mapM (const randomIO) [1 .. size]
+
+aliceInitSwapScript :: SwapHash -> Int -> PublicKey -> PublicKey -> Script
+aliceInitSwapScript swapHash deadlineBob alicePubKey bobPubKey = mainScriptUnsafe $
+  orSigma $ fromVec
+    [ toSigma (getHeight >* int deadlineBob) &&* pk' alicePubKey
+    , pk' bobPubKey &&* (toSigma $ (sha256 $ listAt getBytesVars 0) ==* bytes swapHash)
+    ]
+
+bobInitSwapScript :: SwapHash -> Int -> PublicKey -> PublicKey -> Script
+bobInitSwapScript swapHash deadlineAlice alicePubKey bobPubKey = mainScriptUnsafe $
+  orSigma $ fromVec
+    [ toSigma (getHeight >* int deadlineAlice) &&* pk' bobPubKey
+    , andSigma $ fromVec
+        [ pk' alicePubKey
+        , toSigma $ lengthBytes (listAt getBytesVars 0) <* 33
+        , toSigma $ sha256 (listAt getBytesVars 0) ==* bytes swapHash
+        ]
+    ]
+
