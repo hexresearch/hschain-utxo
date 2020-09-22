937a773c02621f385827605ef966f6c68109322c
Serguey Zefirov <sergueyz@gmail.com>
2020-09-22 21:01:20 +0300

    Keep progress commit
    Now I have exceptions while running node, which is good - it tells what
    to fix.

--- hschain-utxo-pow-node/tests/mining-consensus/run-pow.sh
+++ hschain-utxo-pow-node/tests/mining-consensus/run-pow.sh
@@ -6,5 +6,5 @@ cabal new-build hschain-PoW
 echo ====
 export PK1='"FPY3TBv4c4gKkTsWH68wUAUrXvaLnJnWsDEwhHqotE4T"'
 export PK2='"Gt1aMuZqJ2vnVmrVvgTNsmcYg2aYfc8SbXzfaVFBzE6t"'
-cabal new-exec -- hschain-utxo-pow-node-app pow/node1.yaml --mine --node-secret-env-var PK1 &
-cabal new-exec -- hschain-utxo-pow-node-app pow/node2.yaml --mine --node-secret-env-var PK2
+cabal new-exec -- hschain-utxo-pow-node-app node -c pow/node1.yaml --secret-env-var PK1 -g pow/utxo-genesis --db db &
+cabal new-exec -- hschain-utxo-pow-node-app node -c pow/node2.yaml --secret-env-var PK2 -g pow/utxo-genesis --db db
