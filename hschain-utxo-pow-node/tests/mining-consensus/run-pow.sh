fc44b789de5d8f78a6f86743d48cfab553b2dac7
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2020-10-13 14:35:48 +0300

    Drop flags

--- hschain-utxo-pow-node/tests/mining-consensus/run-pow.sh
+++ hschain-utxo-pow-node/tests/mining-consensus/run-pow.sh
@@ -5,4 +5,4 @@ set -e
 cabal new-build hschain-utxo-pow-node --enable-profiling --enable-library-profiling
 echo ====
 export PK1='"FPY3TBv4c4gKkTsWH68wUAUrXvaLnJnWsDEwhHqotE4T"'
-cabal new-exec -- hschain-utxo-pow-node-app node -c pow/node1-1.yaml --secret-env-var PK1 -g pow/utxo-genesis --db db +RTS -xc
+cabal new-exec -- hschain-utxo-pow-node-app node -c pow/node1-1.yaml --secret-env-var PK1 # +RTS -xc
