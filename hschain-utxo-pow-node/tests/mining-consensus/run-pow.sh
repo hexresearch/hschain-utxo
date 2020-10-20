e1f3e0a9579ec4f1f54154ed88a4769fa73d73b7
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2020-10-20 17:26:33 +0300

    No profiling

--- hschain-utxo-pow-node/tests/mining-consensus/run-pow.sh
+++ hschain-utxo-pow-node/tests/mining-consensus/run-pow.sh
@@ -2,7 +2,8 @@
 set -e
 #trap 'echo TRAP; pkill -P $$' SIGINT SIGTERM
 
-cabal new-build hschain-utxo-pow-node --enable-profiling --enable-library-profiling
+cabal new-build hschain-utxo-pow-node
+#--enable-profiling --enable-library-profiling
 echo ====
 export PK1='"FPY3TBv4c4gKkTsWH68wUAUrXvaLnJnWsDEwhHqotE4T"'
 cabal new-exec -- hschain-utxo-pow-node-app node -c pow/node1-1.yaml --secret-env-var PK1 # +RTS -xc
