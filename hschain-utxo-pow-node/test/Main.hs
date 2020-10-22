fa81ed06ccd234d5279f227484e220b91942031c
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2020-10-22 17:51:04 +0300

    Skeleton for tests of blocks

--- hschain-utxo-pow-node/test/Main.hs
+++ hschain-utxo-pow-node/test/Main.hs
@@ -1,8 +1,10 @@
 
 import Test.Tasty
 import qualified TM.Store
+import qualified TM.Blocks
 
 main :: IO ()
 main = defaultMain $ testGroup "pow-node"
   [ TM.Store.tests
+  , TM.Blocks.tests
   ]
---
+++ hschain-utxo-pow-node/test/TM/Blocks.hs
@@ -0,0 +1,15 @@
+-- |
+module TM.Blocks where
+
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import HSChain.Types.Merkle.Types
+import HSChain.PoW.Types
+import HSChain.PoW.Tests
+import Hschain.Utxo.Pow.App.Types
+
+tests :: TestTree
+tests = testGroup "Running blockchain"
+  [
+  ]
