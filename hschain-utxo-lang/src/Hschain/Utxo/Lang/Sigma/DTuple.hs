5ff83652bdb67db2d0bcd8797b66651135d4cfd7
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2021-01-18 12:57:24 +0300

    Document DH tuple

--- hschain-utxo-lang/src/Hschain/Utxo/Lang/Sigma/DTuple.hs
+++ hschain-utxo-lang/src/Hschain/Utxo/Lang/Sigma/DTuple.hs
@@ -1,10 +1,31 @@
--- | Proof of knowledge of Diffie-Hellman tuple.
--- As described in "Advanced ErgoScript tutorial" (Section 3.3)
+-- |
 --
--- Let @g@, @h@, @u@, @v@ be public group elements. The
--- prover proves the knowledge of @x@ such as
+-- Noninteractive proof that tuple is indeed Diffie-Hellman (DH) tuple.
+-- DH tuple is one of the form
 --
--- > u == g ^ x  &&  v == h ^ x
+-- > (g, h=g^x, u=g^y, v=g^xy)
+--
+-- where @g@ is group generators and @x@,@y@ unknown constant Prover
+-- demonstrates that he knows @y@ without disclosing it. Note that he
+-- doesn't need to know @x@.
+--
+-- == Noninteractive protocol
+--
+-- 1. Prover chooses random @r : ℤ/q@ and keeps it secret. Then he
+--    sends to verifier following commitments:
+--
+-- > t0 = g^r
+-- > t1 = h^r = g^rx
+--
+-- 2. Verifier chooses random challenge @c : ℤ/q@, such @c@ is
+--    generated using hash of @t0@,@t1@ and message.
+--
+-- 3. Prover computes @z = r + cy@ and sends it to verifier.
+--
+-- 4. Verifier accepts proof if
+--
+-- > g^z = t0·u^c = g^r·g^cy
+-- > h^z = t1·v^c = g^rx·g^cxy
 module Hschain.Utxo.Lang.Sigma.DTuple(
     DTuple(..)
   , ProofDTuple(..)
@@ -29,12 +50,12 @@ import Hschain.Utxo.Lang.Sigma.Types
 import qualified Data.ByteString.Lazy as LB
 import qualified Codec.Serialise as CBOR
 
--- | Public input for knowledge of Diffie-Hellman tuple.
+-- | Diffie-Hellmann tuple.
 data DTuple a = DTuple
-  { dtuple'generatorA :: ECPoint a    -- ^ first group element (normally it is the group generator)
-  , dtuple'generatorB :: ECPoint a    -- ^ second group element
-  , dtuple'publicKeyA :: PublicKey a  -- ^ public key for first group element
-  , dtuple'publicKeyB :: PublicKey a  -- ^ public key for second group element
+  { dtuple'generatorA :: ECPoint a    -- ^ group generator @g@
+  , dtuple'generatorB :: ECPoint a    -- ^ @g^x@
+  , dtuple'publicKeyA :: PublicKey a  -- ^ @g^y@
+  , dtuple'publicKeyB :: PublicKey a  -- ^ @g^xy@
   } deriving (Generic)
 
 deriving instance Show (ECPoint a) => Show (DTuple a)
@@ -66,7 +87,7 @@ instance ByteRepr (ECPoint a) => ToJSON (DTuple a) where
 instance ByteRepr (ECPoint a) => FromJSON (DTuple a) where
   parseJSON = defaultParseJSON "DTuple"
 
--- | Proof of knowledge of Diffie-Hellman tuple
+-- | Proof of knowledge of @y@ in Diffie-Hellman tuple
 data ProofDTuple a = ProofDTuple
   { proofDTuple'public      :: DTuple a                      -- ^ public input for the algorithm
   , proofDTuple'commitmentA :: (Commitment a, Commitment a)  -- ^ commitments
